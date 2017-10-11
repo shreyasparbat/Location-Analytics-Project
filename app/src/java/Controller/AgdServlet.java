931b49ceb710562a1b14c91addbb4d565374712c
Amanda Lee Si Hui <amanda.lee.2016@sis.smu.edu.sg>
2017-10-11 18:45:33 +0800

    commit AgdServlet.java

---
+++ app/src/java/Controller/AgdServlet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.dao.AgdDAO;
+
+/**
+ *
+ * @author amanda
+ */
+@WebServlet(name = "AgdServlet", urlPatterns = {"/AgdServlet"})
+public class AgdServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //to take in user inputs
+        String date = request.getParameter("date");
+        String time = request.getParameter("time");
+        String sec = request.getParameter("seconds");
+        String dateTime = date + " " + time + ":" + sec;
+        PrintWriter out = response.getWriter();
+
+        //out.println(dateTime);
+        AgdDAO agdDao = new AgdDAO();
+        try {
+            HashMap<String, ArrayList<String>> groupList = agdDao.detectGroups(dateTime);  // do request shit from the DAO example Agd Fcuntion requt son
+        } catch (ParseException ex) {
+            Logger.getLogger(AgdServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
