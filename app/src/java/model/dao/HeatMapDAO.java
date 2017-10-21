44c336176dd71086e59241dc77256abba3dede11
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2017-10-21 11:02:28 +0800

    Solved heatmap bug which did not allow SRs to be coloured.
    Changed SQL in HeatMapDAO.
    
    Added <ol> tags which were missing in BreakdownUtility
    
    Signed-off-by: Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>

--- app/src/java/model/dao/HeatMapDAO.java
+++ app/src/java/model/dao/HeatMapDAO.java
@@ -24,14 +24,16 @@ public class HeatMapDAO {
     private HashMap<String, Integer> semanticPlaceHeat;
     private Timestamp startDateTime;
     private Timestamp endDateTime;
+    private String level;
 
-    public HeatMapDAO(Timestamp startDateTime, Timestamp endDateTime) {
+    public HeatMapDAO(Timestamp startDateTime, Timestamp endDateTime, String level) {
         semanticPlaceHeat = new HashMap<>();
         this.startDateTime = startDateTime;
         this.endDateTime = endDateTime;
+        this.level = level;
     }
 
-    public HashMap<String, Integer> getAllSemanticPlaceHeat() {
+    public HashMap<String, Integer> getSemanticPlaceHeatFromSpecificFloor() {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -39,27 +41,36 @@ public class HeatMapDAO {
         //Getting HashMap of all students in the SIS building during processing window
         try {
             conn = DBConnection.createConnection();
-            stmt = conn.prepareStatement("select ll.locationid, ll.semanticplace, noOfMacAdd from locationlookup ll "
-                    + "left join "
+            stmt = conn.prepareStatement(
+                    "select t3.sp as 'semanticplace', count(t1.macadd) as 'noOfMacAdd' "
+                    + "from "
                     + "( "
-                    + "    select uniqueMacAddWithMaxTime.latest_time, count(uniqueMacAddWithMaxTime.macaddress) as noOfMacAdd, l.locationid as locid from location l "
-                    + "    inner join "
-                    + "    ( "
-                    + "        select macaddress, max(time) as latest_time "
-                    + "        from location "
-                    + "        group by macaddress "
-                    + ""
-                    + "    ) uniqueMacAddWithMaxTime "
-                    + "    on l.macaddress = uniqueMacAddWithMaxTime.macaddress "
-                    + "    where latest_time >= ? and latest_time < ? "
-                    + "    group by l.locationid "
-                    + "    order by l.locationid "
-                    + ") locIdsOfUniqueMacAddWithMaxTime "
-                    + "on ll.locationid = locIdsOfUniqueMacAddWithMaxTime.locid "
-                    + "order by ll.locationid");
+                    + "select macaddress as 'macadd', time as 'ts', locationid as 'lid' "
+                    + "from location "
+                    + "where time >= ? and time < ? "
+                    + ") as t1 "
+                    + "inner join "
+                    + "( "
+                    + "select macaddress  as 'macadd2', max(time) as 'maxts' from location "
+                    + "where time >= ? and time < ? "
+                    + "group by macadd2"
+                    + ") as t2 "
+                    + "on t1.macAdd = t2.macadd2 and t1.ts = t2.maxts "
+                    + "right outer join "
+                    + "( "
+                    + "select locationid as 'lid2', semanticplace as sp "
+                    + "from locationlookup "
+                    + "where semanticplace like ? "
+                    + ") as t3 "
+                    + "on t1.lid = t3.lid2 "
+                    + "group by t3.sp"
+            );
 
             stmt.setTimestamp(1, startDateTime);
             stmt.setTimestamp(2, endDateTime);
+            stmt.setTimestamp(3, startDateTime);
+            stmt.setTimestamp(4, endDateTime);
+            stmt.setString(5, "%" + level + "%");
 
             rs = stmt.executeQuery();
 
@@ -68,15 +79,8 @@ public class HeatMapDAO {
                 String semanticPlace = rs.getString("semanticplace");
                 int noOfMacAdd = rs.getInt("noOfMacAdd"); //NOTE: no need to check for null cause getInt() returns 0 if value in table is NULL
 
-                //check if semanticPlace already exists in the map
-                if (semanticPlaceHeat.containsKey(semanticPlace)) {
-                    //get the old value, add new value to it then replace in the map
-                    int oldValue = semanticPlaceHeat.get(semanticPlace);
-                    semanticPlaceHeat.replace(semanticPlace, oldValue + noOfMacAdd);
-                } else {
-                    //add to map the place and its initial no of MacAddressess
-                    semanticPlaceHeat.put(semanticPlace, noOfMacAdd);
-                }
+                //put into toReturnMap
+                semanticPlaceHeat.put(semanticPlace, noOfMacAdd);
             }
 
         } catch (SQLException ex) {
@@ -87,6 +91,7 @@ public class HeatMapDAO {
             DBConnection.close(conn, stmt, rs);
         }
 
+        //returning required hashmap
         return semanticPlaceHeat;
     }
 }
